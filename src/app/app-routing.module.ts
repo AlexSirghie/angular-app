77e6c0e6b6f35d1135c1f05e9d9c77f3b146960b
AlexSirghie <alex.sirghie22@gmail.com>
2023-04-21 09:37:36 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< HEAD
 import { EmployeesListComponent } from './employees/employees-list/employees-list.component';
 
 const routes: Routes = [
   { path: "employees-list", component: EmployeesListComponent },
   { path: "**", redirectTo: "employees-list" }
 ];
+=======
+
+const routes: Routes = [];
+>>>>>>> 4667ae8 (initial commit)
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
